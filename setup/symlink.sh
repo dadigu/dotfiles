ffc87e4ca68e2a41af45520dc5f1e03c3e16026b
Daxi Gudjónsson <dg@learningbank.io>
2022-02-01 14:04:36 +0100

    Add karabiner elements config file & symlink

--- setup/symlink.sh
+++ setup/symlink.sh
@@ -2,3 +2,6 @@ ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.my.cnf ~/.my.cnf
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+
+# Symlink karabiner elements shortcut collection
+ln -s ~/dotfiles/karabiner/my_shortcuts.json ~/.config/karabiner/assets/complex_modifications/my_shortcuts.json
