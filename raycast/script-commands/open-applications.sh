b889d21d436e9b94924750893973c9676a6a7804
Daxi Gudjónsson <dg@learningbank.io>
2022-12-19 08:18:10 +0100

    add raycast script commands folder and scripts

---
+++ raycast/script-commands/open-applications.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Applications
+# @raycast.mode silent
+# @raycast.packageName Navigation
+
+# Optional parameters:
+# @raycast.icon icons/folder-applications.png
+
+# Documentation:
+# @raycast.description Opens the Applications folder in the Finder.
+
+open /Applications
---
+++ raycast/script-commands/open-downloads.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Downloads
+# @raycast.mode silent
+# @raycast.packageName Navigation
+
+# Optional parameters:
+# @raycast.icon icons/folder-downloads.png
+
+# Documentation:
+# @raycast.description Opens the Downloads folder in the Finder.
+
+open ~/Downloads
