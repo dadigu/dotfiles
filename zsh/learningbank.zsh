fe1763f43da3a2e876819539bd926cdebb4b34b0
Daxi <dg@learningbank.io>
2023-11-07 12:14:45 +0100

    update lb aliases

--- zsh/learningbank.zsh
+++ zsh/learningbank.zsh
@@ -1,122 +1,15 @@
-RESTORE='\033[0m'
-
-RED='\033[00;31m'
-GREEN='\033[00;32m'
-YELLOW='\033[00;33m'
-BLUE='\033[00;34m'
-PURPLE='\033[00;35m'
-CYAN='\033[00;36m'
-LIGHTGRAY='\033[00;37m'
-
-LRED='\033[01;31m'
-LGREEN='\033[01;32m'
-LYELLOW='\033[01;33m'
-LBLUE='\033[01;34m'
-LPURPLE='\033[01;35m'
-LCYAN='\033[01;36m'
-WHITE='\033[01;37m'
-
-# Check for file changes and run a command
-# https://davidwalsh.name/git-hook-npm-install-package-json-modified
-check_run() {
-    changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
-	echo "$changed_files" | grep --quiet "$1" && eval "$2"
-}
-
+# Add global access to devkit
 alias devkit='~/Development/Learningbank/devkit/devkit'
 
-# Learningbank repository update script
-# This will pull, migrate and npm install all microservices
-# used by the frontend.
+# Quick access learningbank development folder
 function lb() {
     if [ -z $1 ]; then 
-        cd ~/Development/Learningbank && echo -e "\n${LYELLOW}Welcome to Learningbank 🎉 ${RESTORE}\n"
+        cd ~/Development/Learningbank && echo -e "\nWelcome to Learningbank 🎉 \n"
     elif [ $1 = 'start' ]; then
-        #tmuxinator start learningbank -p ~/dotfiles/tmuxinator/learningbank.yml
-        tmuxp load ~/dotfiles/tmuxp/learningbank.yml
+        devkit start
     elif [ $1 = 'stop' ]; then
-        tmux kill-session -t learningbank
-    elif [ $1 = 'update' ]; then
-        # Save current location
-        current_pwd=$(pwd)
-
-        # Auth service
-        cd ~/Development/Learningbank/auth-service
-        branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-        echo -e "\n🔐 ${LCYAN}Updating Auth service.. \t\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-        npm run db:up
-
-        # Api Service
-        cd ~/Development/Learningbank/api-service
-        branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-        echo -e "\n🎛  ${LCYAN}Updating API service.. \t\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-        npm run db:up
-        
-        # Hostconfig service
-        cd ~/Development/Learningbank/hostconfig-service
-        branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-        echo -e "\n⚙️  ${LCYAN}Updating Hostconfig service..\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-        npm run db:up
-
-        # Resource service
-        cd ~/Development/Learningbank/resource-service
-        branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-        echo -e "\n📙 ${LCYAN}Updating Resource service..\t\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-        npm run db:up
-
-        # Client service
-        cd ~/Development/Learningbank/client-service
-        branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-        echo -e "\n ${LCYAN}Updating Client service..\t\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-
-        # Socket service
-        cd ~/Development/Learningbank/socket-service
-        branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-        echo -e "\n🧦 ${LCYAN}Updating Socket service..\t\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-        npm run db:up
-        npm run distribute
-
-        # Insight service
-        cd ~/Development/Learningbank/insight-service
-        branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-        echo -e "\n📊 ${LCYAN}Updating Insight service..\t\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-        npm run db:up
-        npm run build
-
-        # Communication Service
-        cd ~/Development/Learningbank/communication-service
-        echo -e "\n📢 ${LCYAN}Updating Communication service..\t\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-        npm run db:up
-
-        # Mastery Service
-        cd ~/Development/Learningbank/mastery-service
-        echo -e "\n📢 ${LCYAN}Updating Mastert service..\t\t${RESTORE}  ${branch}\n"
-        git pull --ff-only
-        check_run package-lock.json "npm ci"
-        npm run db:up
-
-        # Navigate back to location where script was fired from.
-        cd "$current_pwd"
-
-        echo -e "\n${LYELLOW}You are all set!${RESTORE} 👍 \n"
-
+        devkit stop
     else
-       echo "Unknown parameter '$1'"
+       cd ~/Development/Learningbank/"$1"
     fi
 }
