7c838b0271206ac05b9824bd1c5c3e611db61e3e
Daxi <dg@learningbank.io>
2023-11-07 12:14:12 +0100

    update aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -41,7 +41,15 @@ mtouch () {
     done
 }
 
-# Docker aliases
+# Check for file changes and run a command
+# https://davidwalsh.name/git-hook-npm-install-package-json-modified
+# Usage: check_run package-lock.json "npm ci"
+check_run() {
+    changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+	echo "$changed_files" | grep --quiet "$1" && eval "$2"
+}
+
+# List all running container names
 function dnames-fn {
 	for ID in `docker ps | awk '{print $1}' | grep -v 'CONTAINER'`
 	do
@@ -49,6 +57,7 @@ function dnames-fn {
 	done
 }
 
+# Get all ip addresses from containers
 function dip-fn {
     echo "IP addresses of all named running containers"
 
@@ -61,6 +70,7 @@ function dip-fn {
     unset OUT
 }
 
+# Tail logs on docker container by container name
 function dlf-fn {
     container_name=$1
     tail_len=${2:-50}
@@ -82,11 +92,50 @@ function dlf-fn {
     docker logs -f --tail="$tail_len" "$container_id"
 }
 
+# Exectute command inside docker container
+# Defaults to logging you into container
+# Usage: dex-fn <containerName> <optional: command>
 function dex-fn {
 	container_name=$1
     command=${2:-/bin/sh}
     docker exec -it "$container_name" "$command"
 }
+
+# Run docker tail on multiple containers and open in tmux session
+docker-tail-tmux() {
+    tmux new-session -d -s dockerlogs
+
+    # Calculate the total number of containers
+    num_containers=$#
+
+    # Set the initial pane index
+    pane_index=0
+
+    # Loop through the container names and open logs in separate panes
+    for container_name in "$@"; do
+        # Increment the pane index
+        pane_index=$((pane_index+1))
+
+        # Create a new pane and run Docker logs
+        tmux split-window -h "docker logs -f --tail=50 $container_name"
+
+        # Rename the pane to the container name
+        tmux select-pane -t $pane_index -T "$container_name"
+    done
+
+    # Close the extra empty pane
+    tmux kill-pane -t 0
+
+    # Adjust the pane sizes to evenly distribute them
+    if [ $num_containers -gt 3 ]; then
+        tmux select-layout tiled
+    else
+        tmux select-layout even-horizontal
+    fi
+
+    tmux attach-session -t dockerlogs
+}
+
 
 alias dkps='docker ps'
 alias dkpsa='docker ps -a'
@@ -94,4 +143,4 @@ alias dkip=dip-fn
 alias dknames=dnames-fn
 alias dkex=dex-fn
 alias dklf=dlf-fn
-alias dktail='sh $DOTFILES_PATH/various-scripts/docker-tail.sh'
+alias dktail=docker-tail-tmux
