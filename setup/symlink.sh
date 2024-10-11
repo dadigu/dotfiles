2b154186aade59c7d33d867de65499db5c0241c1
Daxi <dg@learningbank.io>
2024-10-11 07:38:25 +0200

    add yabai and skhd configs and added symlinks

--- setup/symlink.sh
+++ setup/symlink.sh
@@ -3,6 +3,5 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.my.cnf ~/.my.cnf
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.wezterm.lua ~/.wezterm.lua
-
-# Symlink karabiner elements shortcut collection
-ln -s ~/dotfiles/karabiner/my_shortcuts.json ~/.config/karabiner/assets/complex_modifications/my_shortcuts.json
+ln -s ~/dotfiles/.yabairc ~/.yabairc
+ln -s ~/dotfiles/.skhdrc ~/.skhdrc
