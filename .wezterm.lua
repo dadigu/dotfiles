07d8cc4f1560c9c9923e7e6c247c7d33fa86dbce
Daxi <dg@learningbank.io>
2024-10-19 19:13:28 +0200

    update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,10 +10,11 @@ config.color_scheme = 'Catppuccin Frappe'
 -- Font settings
 config.font = wezterm.font("MonoLisa Nerd Font Mono")
 config.font_size = 18
+config.line_height = 1.2
 
 -- Tab bar settings
 config.enable_tab_bar = true
-config.use_fancy_tab_bar = false
+config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
@@ -23,5 +24,8 @@ config.colors = {
         -- background = '#000000',
     }
 }
+
+-- Various
+config.send_composed_key_when_left_alt_is_pressed = true
 
 return config
