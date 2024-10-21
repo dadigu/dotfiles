030b6ba96546051a5d4c7bd1b11a0b26cc93e592
Daxi <dg@learningbank.io>
2024-10-21 23:39:19 +0200

    Add tmux aliases

---
+++ zsh/.config/zsh/aliases/tmux.zsh
@@ -0,0 +1,11 @@
+# Tmux aliases
+
+alias tm='tmux'
+alias tma='tmux attach-session'
+alias tmat='tmux attach-session -t'
+alias tmks='tmux kill-server'
+alias tmksa='tmux kill-session -a'
+alias tml='tmux list-sessions'
+alias tmn='tmux new-session'
+alias tmns='tmux new -s'
+alias tmd='tmux detach-client'
