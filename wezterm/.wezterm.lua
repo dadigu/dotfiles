9110355393ee11bfff8384bf601f5c1c18c89d1e
Daxi <dg@learningbank.io>
2024-11-21 09:30:19 +0100

    update wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,8 +9,9 @@ config.color_scheme = 'Catppuccin Frappe'
 
 -- Font settings
 config.font = wezterm.font("MonoLisa Nerd Font Mono")
-config.font_size = 16
-config.line_height = 1.2
+config.font_size = 15
+config.line_height = 1.3
+config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"} -- No font ligatures
 
 -- Cursor
 config.default_cursor_style = "SteadyBar"
@@ -31,6 +32,5 @@ config.colors = {
 -- Various
 config.window_close_confirmation = 'NeverPrompt'
 config.send_composed_key_when_left_alt_is_pressed = true
-config.harfbuzz_features = {"calt=0", "clig=0", "liga=0"} -- No font ligatures
 
 return config
