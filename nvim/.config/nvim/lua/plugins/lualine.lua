9ec9cf785a19cd553609858e99e506ca053bcc73
Daxi <dg@learningbank.io>
2025-05-08 14:05:16 +0200

    modify nvim lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,7 +1,11 @@
 return {
   "nvim-lualine/lualine.nvim",
   opts = {
+    options = {
+      section_separators = { left = "", right = "" },
+    },
     sections = {
+      lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
       -- Remove clock from lualine
       lualine_z = {},
     },
