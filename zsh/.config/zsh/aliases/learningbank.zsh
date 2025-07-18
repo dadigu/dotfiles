153660d3cd6b81e210c310fbf77d4a6eb6d21257
Daxi <dg@learningbank.io>
2025-07-18 08:21:45 +0200

    remove unused zsh alias

--- zsh/.config/zsh/aliases/learningbank.zsh
+++ zsh/.config/zsh/aliases/learningbank.zsh
@@ -1,17 +1,3 @@
 # Add global access to devkit
 alias devkit='~/Development/Learningbank/devkit/devkit'
 
-# Quick access learningbank development folder
-function lb() {
-    if [ -z $1 ]; then 
-        cd ~/Development/Learningbank/devkit/services && echo -e "\nWelcome to Learningbank 🎉 \n" && lla
-    elif [ $1 = 'start' ]; then
-        devkit start
-    elif [ $1 = 'stop' ]; then
-        devkit stop
-    elif [ $1 = 'app' ]; then
-	cd ~/Development/Learningbank/app
-    else
-       cd ~/Development/Learningbank/devkit/services/"$1"
-    fi
-}
