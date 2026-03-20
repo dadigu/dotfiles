ba2075fb18c526f04713d82ef9d488251aeaedb8
Daxi <dg@learningbank.io>
2026-03-20 19:56:29 +0100

    tmux: add keybind for quick worktree switch

---
+++ various-scripts/tmux-worktree-picker.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+APP="$HOME/Development/Learningbank/app"
+
+sel=$(cd "$APP/repo" && git worktree list | awk '{print $1}' | while read -r p; do
+  printf "%s\t%s\n" "${p#$APP/}" "$p"
+done | fzf --reverse --header="Select worktree" --with-nth=1 --delimiter=$'\t')
+
+[ -z "$sel" ] && exit 0
+
+dir=$(echo "$sel" | cut -f2)
+name=$(echo "$sel" | cut -f1)
+
+tmux new-window -n "app: $name" -c "$dir" \; \
+  split-window -v -c "$dir" \; \
+  split-window -h -c "$dir" \; \
+  select-pane -t 1
